a324e3cb2e04218310fcc05d92a0545483344bc7
Kaike Castro <42541528+kaikecc@users.noreply.github.com>
2019-08-29 19:14:46 -0300

    Create mux2x1.vhd

---
+++ Circuitos Digitais/ATIVIDADES2019-2/ATIVIDADE-5/CODE/mux2x1.vhd
@@ -0,0 +1,15 @@
+entity mux2x1 is
+  
+  port( Ea, Eb, SEL: in bit;
+    
+        Saida : out bit);
+end;
+
+architecture ckt of mux2x1 is
+  
+ begin
+  
+  Saida <= (Ea and not(SEL)) or ( Eb and SEL);
+  
+end ckt;
+
