87cd148b63e7279f717a63e94723592910e0002b
Kaike Castro <42541528+kaikecc@users.noreply.github.com>
2019-08-30 19:47:55 -0300

    Update bloco.vhd

--- Circuitos Digitais/ATIVIDADES2019-2/ATIVIDADE-3/CODE/bloco.vhd
+++ Circuitos Digitais/ATIVIDADES2019-2/ATIVIDADE-3/CODE/bloco.vhd
@@ -7,10 +7,9 @@ end;
 
  architecture CKT of bloco is 
  begin
- S(3) <= (not(A(3)) and A(2) and A(0)) or (not(A(3)) and A(2) and A(1)) or (A(3) and not(A(2)) and not(A(1)));
- S(2) <= (not(A(3)) and A(2) and not(A(1)) and not (A(0)));
- S(1) <= (not(A(3)) and not (A(2)) and A(1)) or (not(A(3)) and A(1) and A(0)) or (A(3) and not(A(2)) and not(A(1)) and not(A(0)));
- S(0) <= (not(A(3)) and not(A(2)) and A(0)) or (not(A(3)) and A(2) and A(1) and not(A(0))) or (A(3) and not(A(2)) and not(A(1)) and not(A(0)));
-
+        S(3) <= A(3) OR (A(2) AND A(0)) OR (A(2) AND A(1));
+        S(2) <= (A(3) AND A(0)) OR (A(2) AND (not A(1)) AND (not A(0)));        
+        S(1) <= ((not A(2)) AND A(1)) OR (A(1) AND A(0)) OR (A(3) AND (not A(0)));        
+        S(0) <= (A(3) AND (not A(0))) OR ((not A(3)) AND (not A(2)) AND A(0)) OR (A(2) AND A(1) AND (not A(0)));
   
 end CKT;
