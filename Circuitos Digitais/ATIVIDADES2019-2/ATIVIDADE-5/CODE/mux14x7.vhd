abdf01d376a40b67dfaccd3155c91e29ee57f536
Kaike Castro <42541528+kaikecc@users.noreply.github.com>
2019-08-30 19:30:10 -0300

    Update mux14x7.vhd

--- Circuitos Digitais/ATIVIDADES2019-2/ATIVIDADE-5/CODE/mux14x7.vhd
+++ Circuitos Digitais/ATIVIDADES2019-2/ATIVIDADE-5/CODE/mux14x7.vhd
@@ -1,17 +1,15 @@
 entity mux14x7 is
   
-  port( A1, B1: in bit_vector(6 downto 0); 
-  SEL1: in bit;
-    
-        S1 : out bit_vector(6 downto 0));
+  port(SELI: in bit;
+  AI, BI: in bit_vector(6 downto 0);  
+  SI :   out bit_vector(6 downto 0));
 end;
 
-architecture ckt of mux14x7 is
+architecture ckto of mux14x7 is
 
 component mux2x1 is
   
-  port( Ea, Eb, SEL: in bit;
-    
+port( Ea, Eb, SEL: in bit;    
         Saida : out bit);
 end component;
 
@@ -20,51 +18,54 @@ begin
 
 M0: mux2x1 port map(
   
-        Ea => A1(0),
-        Eb => B1(0),
-        SEL => SEL1,
+        Ea => AI(0),
+        Eb => BI(0),
+        SEL => SELI,
         Saida => AUXM(0));
         
 M1: mux2x1 port map(
   
-        Ea => A1(1),
-        Eb => B1(1),
-        SEL => SEL1,
+        Ea => AI(1),
+        Eb => BI(1),
+        SEL => SELI,
         Saida => AUXM(1));
+        
 M2: mux2x1 port map(
   
-        Ea => A1(0),
-        Eb => B1(0),
-        SEL => SEL1,
-        Saida => AUXM(0));
+        Ea => AI(2),
+        Eb => BI(2),
+        SEL => SELI,
+        Saida => AUXM(2));
+        
 M3: mux2x1 port map(
   
-        Ea => A1(3),
-        Eb => B1(3),
-        SEL => SEL1,
+        Ea => AI(3),
+        Eb => BI(3),
+        SEL => SELI,
         Saida => AUXM(3));
 M4: mux2x1 port map(
   
-        Ea => A1(4),
-        Eb => B1(4),
-        SEL => SEL1,
+        Ea => AI(4),
+        Eb => BI(4),
+        SEL => SELI,
         Saida => AUXM(4));
+        
 M5: mux2x1 port map(
   
-        Ea => A1(5),
-        Eb => B1(5),
-        SEL => SEL1,
+        Ea => AI(5),
+        Eb => BI(5),
+        SEL => SELI,
         Saida => AUXM(5));
-M6: mux2x1 port map(
-  
-  
-        Ea => A1(6),
-        Eb => B1(6),
-        SEL => SEL1,
-        Saida => AUXM(6));
-S1 <= AUXM;      
         
+M6: mux2x1 port map( 
   
-  
-end ckt;
+        Ea => AI(6),
+        Eb => BI(6),
+        SEL => SELI,
+        Saida => AUXM(6));
+        
+SI(6 downto 0) <= AUXM(6 downto 0);      
+        
+    
+end ckto;
 
