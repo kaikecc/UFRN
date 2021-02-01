d6264d56facbd1dee621eab88727c2432dc9b0c4
Kaike Castro <42541528+kaikecc@users.noreply.github.com>
2021-02-01 18:28:00 -0300

    qUASE FIM

--- 8º PERÍODO/SISTEMA DIGITAIS/PROBLEMA 1 - IMPLEMENTACAO/multiplicador4bit.vhd
+++ 8º PERÍODO/SISTEMA DIGITAIS/PROBLEMA 1 - IMPLEMENTACAO/multiplicador4bit.vhd
@@ -19,16 +19,26 @@ port( S81, S82: in std_logic_vector(7 downto 0);
         
 end component;
 
-signal OUT_S0, OUT_S1, OUT_S2 : std_logic_vector(7 downto 0);
+signal OUT_S0, OUT_S1, OUT_S2, AUXS1, AUXS2 : std_logic_vector(7 downto 0);
 
 begin
 
+AUXS1(0) <= M_A(0) and M_B(0);
+AUXS1(1) <= M_A(1) and M_B(0);
+AUXS1(2) <= M_A(2) and M_B(0);
+AUXS1(3) <= M_A(3) and M_B(0);
+
+AUXS1(4) <= M_A(0) and M_B(1);
+AUXS1(5) <= M_A(1) and M_B(1);
+AUXS1(6) <= M_A(2) and M_B(1);
+AUXS1(7) <= M_A(3) and M_B(1);
+
 SUM0: somador8bit port map(
   
-  S81(0) => M_A(0) and M_B(0),
-  S81(1) => M_A(1) and M_B(0),
-  S81(2) => M_A(2) and M_B(0),
-  S81(3) => M_A(3) and M_B(0),
+  S81(0) => AUXS1(0),
+  S81(1) => AUXS1(1),
+  S81(2) => AUXS1(2),
+  S81(3) => AUXS1(3),
   S81(4) => '0',
   S81(5) => '0',
   S81(6) => '0',
@@ -36,10 +46,10 @@ SUM0: somador8bit port map(
   
   
   S82(0) => '0',
-  S82(1) =>  M_A(0) and M_B(1),
-  S82(2) =>  M_A(1) and M_B(1),
-  S82(3) =>  M_A(2) and M_B(1),
-  S82(4) =>  M_A(3) and M_B(1),
+  S82(1) =>  AUXS1(4),
+  S82(2) =>  AUXS1(5),
+  S82(3) =>  AUXS1(6),
+  S82(4) =>  AUXS1(7),
   S82(5) => '0',
   S82(6) => '0',
   S82(7) => '0',
@@ -47,6 +57,17 @@ SUM0: somador8bit port map(
   Ce => '0',
   Z => OUT_S0);
  
+AUXS2(0) <= M_A(0) and M_B(2);
+AUXS2(1) <= M_A(1) and M_B(2);
+AUXS2(2) <= M_A(2) and M_B(2);
+AUXS2(3) <= M_A(3) and M_B(2);
+
+AUXS2(4) <= M_A(0) and M_B(3);
+AUXS2(5) <= M_A(1) and M_B(3);
+AUXS2(6) <= M_A(2) and M_B(3);
+AUXS2(7) <= M_A(3) and M_B(3);
+
+
 SUM1: somador8bit port map(
   
   S81(0) => OUT_S0(0),
@@ -61,10 +82,10 @@ SUM1: somador8bit port map(
   
   S82(0) => '0',
   S82(1) => '0',
-  S82(2) =>  M_A(0) and M_B(2),
-  S82(3) =>  M_A(1) and M_B(2),
-  S82(4) =>  M_A(2) and M_B(2),
-  S82(5) =>  M_A(3) and M_B(2),
+  S82(2) =>  AUXS2(0),
+  S82(3) =>  AUXS2(1),
+  S82(4) =>  AUXS2(2),
+  S82(5) =>  AUXS2(3),
   S82(6) => '0',
   S82(7) => '0',
   
@@ -86,10 +107,10 @@ SUM2: somador8bit port map(
   S82(0) => '0',
   S82(1) => '0',
   S82(2) => '0',
-  S82(3) =>  M_A(0) and M_B(3),
-  S82(4) =>  M_A(1) and M_B(3),
-  S82(5) =>  M_A(2) and M_B(3),
-  S82(6) =>  M_A(3) and M_B(3),
+  S82(3) =>  AUXS2(4),
+  S82(4) =>  AUXS2(5),
+  S82(5) =>  AUXS2(6),
+  S82(6) =>  AUXS2(7),
   S82(7) => '0',
   
   Ce => '0',
