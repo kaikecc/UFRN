e9ed3d588c1628311824d5e1b7a3926d8ee924d4
Kaike Castro <42541528+kaikecc@users.noreply.github.com>
2021-09-16 13:40:10 -0300

    Update mapaPy.py

--- 10º PERÍODO/mapaPy.py
+++ 10º PERÍODO/mapaPy.py
@@ -4,14 +4,19 @@ from sklearn import preprocessing
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
-
+import pandas as pd
+from google.colab import drive
+drive.mount('/content/drive') 
 
 path = '/content/mapa.csv'
 mapa = pd.read_csv(path)
-mapa.columns=['x', 'y']
+mapa.columns=['x', 'y','z']
 m = mapa[mapa.x != -9]
 
-A = np.zeros((20, 20))
+#A = np.zeros((20, 20))
+A = np.full((20, 20), 0.5)
+
+
 
 for index, row in m.iterrows():
    
@@ -27,8 +32,14 @@ for index, row in m.iterrows():
    else:                                   # Quarto Quadrante
      i = 10 + math.ceil((row['y']/(-2.5))*10)
      j = 10 + math.ceil((row['x']/(2.5))*10)
+   
+   if row['z'] == 1:
+      A[i][j] =  A[i][j] + 0.00125
+   else:
+      A[i][j] = 0 #+ A[i][j]
 
-   A[i][j] = A[i][j] + 0.5
+
+              
 
 A = preprocessing.normalize(A, norm="l1")
 
